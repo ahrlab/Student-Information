1ba3310d9202e5e1df5e1912e29fe7d5cbf4a153
ahrlab <ahrlab@live.com>
2015-09-24 17:42:40 +0600

    git init

---
+++ Student Information/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Student_Information
+{
+
+    class Stuinfo
+    {
+        public string name;
+        public int id;
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stuinfo sti = new Stuinfo();
+            sti.name = "Demo";
+            sti.id = 1234;
+            Console.WriteLine(sti.name);
+            Console.WriteLine(sti.id);
+            Console.ReadKey();
+        }
+    }
+}
